3e151215a52659de181c8e8ed82e181de854e4a1
alu364 <alu364@laboratorios.local>
2026-05-27 20:51:35 -0300

    Agregué sprites para todas las direcciones y agregué las animaciones al Animator Controller

--- Assets/PlayerMovement2D.cs
+++ Assets/PlayerMovement2D.cs
@@ -5,19 +5,26 @@ public class PlayerMovement2D : MonoBehaviour
     [SerializeField] private float speed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
+
     private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal"); // A/D
-        movement.y = Input.GetAxisRaw("Vertical");   // W/S
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+
+        bool isMoving = movement != Vector2.zero;
+
+        animator.SetBool("isMoving", isMoving);
     }
 
     void FixedUpdate()
