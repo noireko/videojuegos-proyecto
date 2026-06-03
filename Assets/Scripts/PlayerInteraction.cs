e36d2d9a43e323e3661a0f034e23aee36463f66c
noireko <agustinfernandezrolon@gmail.com>
2026-06-03 16:09:26 -0300

    Cambie hitInterval

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -5,8 +5,12 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] float interactRadius = 1.5f;
     [SerializeField] LayerMask interactableLayer;
 
+    [SerializeField] float hitInterval = 0.5f;
+
     IInteractable currentTarget;
     PlayerMovement playerMovement;
+
+    private float hitTimer = 0f;
 
     void Start()
     {
@@ -16,14 +20,24 @@ public class PlayerInteraction : MonoBehaviour
     void Update()
     {
         Collider2D hit = Physics2D.OverlapCircle(
-            transform.position, interactRadius, interactableLayer);
+            transform.position,
+            interactRadius,
+            interactableLayer);
 
         currentTarget = hit != null ? hit.GetComponent<IInteractable>() : null;
 
-        if (Input.GetKeyDown(KeyCode.E) && currentTarget != null)
+        if (hitTimer > 0)
+            hitTimer -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E) && currentTarget != null)
         {
             playerMovement.SetLocked(true);
-            currentTarget.Interact();
+
+            if (hitTimer <= 0)
+            {
+                currentTarget.Interact();
+                hitTimer = hitInterval;
+            }
         }
 
         if (Input.GetKeyUp(KeyCode.E))
