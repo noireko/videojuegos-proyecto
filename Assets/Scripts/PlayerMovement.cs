d057dea2c0ad087f33e3ac4ced15b93febe9731c
noireko <agustinfernandezrolon@gmail.com>
2026-06-03 13:30:14 -0300

    Agregué un cooldown antes de aplicarse isMoving al moverse para que el personaje se quede viendo en la dirección que caminaba

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 movement;
+
+    private int lastX = 0;
+    private int lastY = -1;
+
+    private float idleDelay = 0.1f;
+    private float idleTimer = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        animator.SetInteger("moveX", lastX);
+        animator.SetInteger("moveY", lastY);
     }
 
     void Update()
     {
         int x = (int)Input.GetAxisRaw("Horizontal");
         int y = (int)Input.GetAxisRaw("Vertical");
-
         movement = new Vector2(x, y).normalized;
 
-        bool isMoving = movement != Vector2.zero;
-
-        animator.SetBool("isMoving", isMoving);
-
-        if (isMoving)
+        if (x != 0 || y != 0)
         {
-            animator.SetInteger("moveX", x);
-            animator.SetInteger("moveY", y);
+            bool comingFromIdle = idleTimer <= 0f;
+            bool isFullDiagonal = x != 0 && y != 0;
+
+            if (isFullDiagonal || comingFromIdle)
+            {
+                lastX = x;
+                lastY = y;
+                animator.SetInteger("moveX", lastX);
+                animator.SetInteger("moveY", lastY);
+            }
+
+            idleTimer = idleDelay;
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            idleTimer -= Time.deltaTime;
+            if (idleTimer <= 0f)
+            {
+                animator.SetBool("isMoving", false);
+            }
         }
     }
 
