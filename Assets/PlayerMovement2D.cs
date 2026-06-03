4a9f408293b636276560e5509923fca6dbd78616
noireko <agustinfernandezrolon@gmail.com>
2026-06-03 11:47:50 -0300

    Agregue una clase para interactuar con objetos

--- Assets/PlayerMovement2D.cs
+++ Assets/PlayerMovement2D.cs
@@ -31,3 +31,28 @@ public class PlayerMovement2D : MonoBehaviour
         rb.linearVelocity = movement * speed;
     }
 }
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] float interactRadius = 1.5f;
+    [SerializeField] LayerMask interactableLayer;
+
+    IInteractable currentTarget;
+
+    void Update()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(
+            transform.position, interactRadius, interactableLayer);
+
+        currentTarget = hit != null ? hit.GetComponent<IInteractable>() : null;
+
+        if (Input.GetKeyDown(KeyCode.E) && currentTarget != null)
+            currentTarget.Interact();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactRadius);
+    }
+}
