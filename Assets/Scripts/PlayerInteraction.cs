b86c1242089e4affa732a43317d4c75e7d87ad68
noireko <agustinfernandezrolon@gmail.com>
2026-06-03 12:44:00 -0300

    Para que mirar la direccion donde uno camina al dejar de apretar la tecla, importe los sprites de todas las direcciones, los .anim para meter en el anim controller, y modifique el script PlayerMovement

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,37 +1,5 @@
 using UnityEngine;
 
-public class PlayerMovement2D : MonoBehaviour
-{
-    [SerializeField] private float speed = 5f;
-
-    private Rigidbody2D rb;
-    private Animator animator;
-    private Vector2 movement;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        int x = (int)Input.GetAxisRaw("Horizontal");
-        int y = (int)Input.GetAxisRaw("Vertical");
-
-        movement = new Vector2(x, y).normalized;
-
-        animator.SetBool("isMoving", movement != Vector2.zero);
-        animator.SetInteger("moveX", x);
-        animator.SetInteger("moveY", y);
-    }
-
-    void FixedUpdate()
-    {
-        rb.linearVelocity = movement * speed;
-    }
-}
-
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] float interactRadius = 1.5f;
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        int x = (int)Input.GetAxisRaw("Horizontal");
+        int y = (int)Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(x, y).normalized;
+
+        bool isMoving = movement != Vector2.zero;
+
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+        {
+            animator.SetInteger("moveX", x);
+            animator.SetInteger("moveY", y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * speed;
+    }
+}
