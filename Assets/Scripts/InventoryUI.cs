972da495a172a0ce5533dcb88c28afc65ee08dd0
noireko <agustinfernandezrolon@gmail.com>
2026-06-05 21:18:38 -0300

    Guardado

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,14 +1,26 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    [System.Serializable]
+    public class ItemUI
+    {
+        public string itemName;
+        public Image icon;
+        public TMP_Text amountText;
+    }
+
     [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private ItemUI[] itemUIs;
 
     private bool isOpen = false;
 
     void Start()
     {
         inventoryPanel.SetActive(false);
+        UpdateUI();
     }
 
     void Update()
@@ -17,6 +29,24 @@ public class InventoryUI : MonoBehaviour
         {
             isOpen = !isOpen;
             inventoryPanel.SetActive(isOpen);
+            UpdateUI();
+        }
+    }
+
+    public void UpdateUI()
+    {
+        foreach (ItemUI itemUI in itemUIs)
+        {
+            int amount = 0;
+
+            if (Inventory.instance.Items.ContainsKey(itemUI.itemName))
+                amount = Inventory.instance.Items[itemUI.itemName];
+
+            itemUI.amountText.text = amount.ToString();
+
+            bool hasItem = amount > 0;
+            itemUI.icon.enabled = hasItem;
+            itemUI.amountText.enabled = hasItem;
         }
     }
 }
