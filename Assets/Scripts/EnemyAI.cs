3fc585d39992ca99cee41412a43f41eb66f435f6
noireko <agustinfernandezrolon@gmail.com>
2026-06-23 19:14:47 -0300

    Modifique el script de EnemyIA

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamageable
 {
     [SerializeField] private float speed = 2f;
     [SerializeField] private float detectionRange = 4f;
     [SerializeField] private float stopDistance = 0.5f;
+    [SerializeField] private int maxHP = 100;
 
+    private int currentHP;
     private Transform player;
     private Rigidbody2D rb;
     private Vector2 movement;
 
     void Start()
     {
+        currentHP = maxHP;
         rb = GetComponent<Rigidbody2D>();
-
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         if (playerObject != null)
             player = playerObject.transform;
@@ -40,5 +42,14 @@ public class EnemyAI : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+        Debug.Log($"{gameObject.name} recibió {amount} daño. HP: {currentHP}/{maxHP}");
+
+        if (currentHP <= 0)
+            Destroy(gameObject);
     }
 }
