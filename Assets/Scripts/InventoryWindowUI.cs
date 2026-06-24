3cd19904d32e30f5fe754f318b03c43a03ae463b
noireko <agustinfernandezrolon@gmail.com>
2026-06-24 02:09:51 -0300

    Script para construir modificado, agrgue botones para la interfaz

--- Assets/Scripts/InventoryWindowUI.cs
+++ Assets/Scripts/InventoryWindowUI.cs
@@ -2,72 +2,19 @@ using UnityEngine;
 
 public class InventoryWindowUI : MonoBehaviour
 {
-    [Header("Paneles")]
-    [SerializeField] private GameObject inventoryPanel;
     [SerializeField] private GameObject constructionPanel;
-
-    private bool isOpen = false;
 
     void Start()
     {
-        inventoryPanel.SetActive(false);
         constructionPanel.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            ToggleWindow();
+            bool abierto = constructionPanel.activeSelf;
+            constructionPanel.SetActive(!abierto);
         }
-    }
-
-    public void ToggleWindow()
-    {
-        isOpen = !isOpen;
-
-        if (isOpen)
-        {
-            ShowInventory();
-        }
-        else
-        {
-            inventoryPanel.SetActive(false);
-            constructionPanel.SetActive(false);
-        }
-    }
-
-    public void ShowInventory()
-    {
-        isOpen = true;
-
-        inventoryPanel.SetActive(true);
-        constructionPanel.SetActive(false);
-    }
-
-    public void ShowConstruction()
-    {
-        isOpen = true;
-
-        inventoryPanel.SetActive(false);
-        constructionPanel.SetActive(true);
-    }
-
-    public void ToggleInventoryConstruction()
-    {
-        isOpen = true;
-
-        bool showingInventory = inventoryPanel.activeSelf;
-
-        inventoryPanel.SetActive(!showingInventory);
-        constructionPanel.SetActive(showingInventory);
-    }
-
-    public void CloseWindow()
-    {
-        isOpen = false;
-
-        inventoryPanel.SetActive(false);
-        constructionPanel.SetActive(false);
     }
 }
