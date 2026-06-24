83198a7728a33e8ed47cb8c1a54347578f2cfe50
noireko <agustinfernandezrolon@gmail.com>
2026-06-24 03:06:03 -0300

    Agregue el HUD para la barra de vida, y cambie parametros para que el jugador guarde el punto de respawn en la cama

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int maxHP = 100;
+    [SerializeField] Slider healthBar;
+
     int currentHP;
 
     void Start()
     {
         currentHP = maxHP;
+        ActualizarBarra();
     }
 
     public void TakeDamage(int amount)
     {
         currentHP -= amount;
         Debug.Log($"Jugador recibió {amount} daño. HP: {currentHP}/{maxHP}");
+        ActualizarBarra();
 
         if (currentHP <= 0)
             Morir();
@@ -23,6 +28,13 @@ public class PlayerHealth : MonoBehaviour
     {
         Debug.Log("Jugador murió, respawneando...");
         currentHP = maxHP;
+        ActualizarBarra();
         RespawnManager.instance.Respawn();
+    }
+
+    void ActualizarBarra()
+    {
+        if (healthBar != null)
+            healthBar.value = currentHP;
     }
 }
